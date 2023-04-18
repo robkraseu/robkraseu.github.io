ce55ccda3a38f7b5712af9e1dcd030dea8fa4a59
Rob Kras <robkraseu@gmail.com>
2023-04-18 21:01:02 +0200

    New page v2.0

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Comp sci student", "Data scientist", "Software developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
