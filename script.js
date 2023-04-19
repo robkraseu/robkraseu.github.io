6d171e8072000e0f1b8e9b5921e3b2512967718d
Rob Kras <robkraseu@gmail.com>
2023-04-19 11:13:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Comp sci student", "Data scientist", "Software developer"],
+    strings: ["BSc student", "Data scientist", "Software developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
