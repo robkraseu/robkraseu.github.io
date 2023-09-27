e70a4e4ae443dea55d4af111e3428a82388dfefe
robkraseu <robkraseu@gmail.com>
2023-09-27 13:01:39 +0200

    v3

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["From the Netherlands!", "Bachelors CS student!", "Passionate about AI and data science!"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 2000,
+    loop: true
+})
+
