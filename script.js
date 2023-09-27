65dded65543892b5787f947445dab34b9164ee9f
robkraseu <robkraseu@gmail.com>
2023-09-27 13:01:12 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["CS student", "Data scientist", "Software developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
-
